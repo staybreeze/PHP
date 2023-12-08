314452504a269764c039cb0357881c3c9452ff0c
staybreeze <wunshengliao@gmail.com>
2023-12-08 10:35:47 +0800

    feat:完成RWD

--- myProject/online_store/index.php
+++ myProject/online_store/index.php
@@ -15,7 +15,10 @@ include_once "./api/db.php";
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
     integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" media="screen and (max-width: 1100px)" href="./css/smallscreen.css">
+    <!-- <link rel="stylesheet" media="screen and (max-width: 400px)" href="./css/7th_screen.css"> -->
+    <!-- <link rel="stylesheet" media="screen and (max-width: 800px)" href="./css/6th_screen.css"> -->
+    <!-- <link rel="stylesheet" media="screen and (max-width  : 1200px)" href="./css/5th_screen.css"> -->
+    <!-- <link rel="stylesheet" media="screen and (max-width: 1200px)" href="./css/smallscreen.css"> -->
 <link rel="stylesheet" media="screen and (max-width: 1400px)" href="./css/middlescreen.css">
 <link rel="stylesheet" media="screen and (max-width:1899px)" href="./css/2nd_screen.css">
 <link rel="stylesheet" media="screen and (min-width: 1900px)" href="./css/bigscreen.css">
